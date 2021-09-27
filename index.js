33a2f9e669ea6aabd997b9fb8308f8461ef7e338
Nikolyovin <37635709+Nikolyovin@users.noreply.github.com>
2021-09-27 09:18:38 +0300

    feat: layout without adapt

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const swiper = new Swiper('.swiper', {
 
    },
 });
+
+const progressBar = document.querySelector('.progress-bar');
+
+progressBar.addEventListener('input', function () {
+   const value = this.value;
+   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
+
+const progressVolume = document.querySelector('.progress-volume');
+
+progressVolume.addEventListener('input', function () {
+   const value = this.value;
+   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
