6d3701e8fbf522af62b27397abe0fd3240cfec8a
Nikolyovin <37635709+Nikolyovin@users.noreply.github.com>
2021-09-28 16:29:43 +0300

    add gallery.js

---
+++ gallery.js
@@ -0,0 +1,27 @@
+const pictureInnerContainer = document.querySelector('.picture-inner-container');
+
+
+let srcGallery = [`assets/img/galery1.jpg`, `assets/img/galery2.jpg`,
+   `assets/img/galery3.jpg`, `assets/img/galery4.jpg`, `assets/img/galery5.jpg`,
+   `assets/img/galery6.jpg`, `assets/img/galery7.jpg`, `assets/img/galery8.jpg`,
+   `assets/img/galery9.jpg`, `assets/img/galery10.jpg`, `assets/img/galery11.jpg`,
+   `assets/img/galery12.jpg`, `assets/img/galery13.jpg`, `assets/img/galery14.jpg`,
+   `assets/img/galery15.jpg`];
+
+
+
+function shuffle(array) {
+   array.sort(() => Math.random() - 0.5);
+   return array;
+}
+shuffle(srcGallery);
+
+
+
+for (i = 0; i < srcGallery.length; i++) {
+
+   const img = document.createElement('img');
+   img.classList.add('gallery-img')
+   img.src = srcGallery[i];
+   pictureInnerContainer.append(img);
+}
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-welcome', {
    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next-welcome',
+      prevEl: '.swiper-button-prev-welcome',
    },
    pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination-welcome',
       clickable: true,
+      dynamicBullets: true,
 
    },
+
+   loop: true,
+   autoplay: {
+      delay: 3000,
+      stopOnlastSlide: true,
+      disableOnInteraction: false,
+   },
+   speed: 800,
+
 });
 
 const progressBar = document.querySelector('.progress-bar');
@@ -23,3 +33,17 @@ progressVolume.addEventListener('input', function () {
    const value = this.value;
    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
 })
+
+
+const swiperVideo = new Swiper(".swiper-video", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   pagination: {
+      el: ".swiper-video-pagination",
+      clickable: true,
+   },
+   navigation: {
+      nextEl: '.swiper-video-button-next',
+      prevEl: '.swiper-video-button-prev',
+   },
+});
