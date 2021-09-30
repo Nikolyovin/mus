1acd3fc0aa38adb5ff841be8d9721c862b6da7db
Nikolyovin <37635709+Nikolyovin@users.noreply.github.com>
2021-09-30 16:10:20 +0300

    feat: add input

--- gallery.js
+++ gallery.js
@@ -1,6 +1,5 @@
 const pictureInnerContainer = document.querySelector('.picture-inner-container');
 
-
 let srcGallery = [`assets/img/galery1.jpg`, `assets/img/galery2.jpg`,
    `assets/img/galery3.jpg`, `assets/img/galery4.jpg`, `assets/img/galery5.jpg`,
    `assets/img/galery6.jpg`, `assets/img/galery7.jpg`, `assets/img/galery8.jpg`,
@@ -8,18 +7,13 @@ let srcGallery = [`assets/img/galery1.jpg`, `assets/img/galery2.jpg`,
    `assets/img/galery12.jpg`, `assets/img/galery13.jpg`, `assets/img/galery14.jpg`,
    `assets/img/galery15.jpg`];
 
-
-
 function shuffle(array) {
    array.sort(() => Math.random() - 0.5);
    return array;
 }
 shuffle(srcGallery);
 
-
-
 for (i = 0; i < srcGallery.length; i++) {
-
    const img = document.createElement('img');
    img.classList.add('gallery-img')
    img.src = srcGallery[i];
