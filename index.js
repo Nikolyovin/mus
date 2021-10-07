277e5e5dddc0dbcb1ae554ddb900a444ceefb1b7
Nikolyovin <37635709+Nikolyovin@users.noreply.github.com>
2021-10-07 14:30:33 +0300

    feat: add form buy

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-const swiper = new Swiper('.swiper-welcome', {
-   navigation: {
-      nextEl: '.swiper-button-next-welcome',
-      prevEl: '.swiper-button-prev-welcome',
-   },
-   pagination: {
-      el: '.swiper-pagination-welcome',
-      clickable: true,
-      dynamicBullets: true,
+import gallery from './gallery.js';
+gallery();
 
-   },
-
-   loop: true,
-   autoplay: {
-      delay: 3000,
-      stopOnlastSlide: true,
-      disableOnInteraction: false,
-   },
-   speed: 800,
-
-});
-
-const progressBar = document.querySelector('.progress-bar');
-
-progressBar.addEventListener('input', function () {
-   const value = this.value;
-   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
-})
-
-const progressVolume = document.querySelector('.progress-volume');
-
-progressVolume.addEventListener('input', function () {
-   const value = this.value;
-   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
-})
+import * as formFn from './form.js';
+window.openForm = formFn.openForm;
+window.closeForm = formFn.closeForm;
 
 
-const swiperVideo = new Swiper(".swiper-video", {
-   slidesPerView: 3,
-   spaceBetween: 30,
-   pagination: {
-      el: ".swiper-video-pagination",
-      clickable: true,
-   },
-   navigation: {
-      nextEl: '.swiper-video-button-next',
-      prevEl: '.swiper-video-button-prev',
-   },
-});
+
